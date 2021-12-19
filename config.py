7353f0ec8817572388e17ce74d9a93f120b13d99
tanveer muttaqueen <1505002.tm@ugrad.cse.buet.ac.bd>
2021-12-19 13:22:17 +0600

    added some coins

---
+++ config.py
@@ -0,0 +1,27 @@
+
+CRYPTO_LIST = [ 'Bitcoin', 'Ethereum', 'Cardano', 'Enjin Coin', 'Ripple', 'Polygon', 'Solana' ]
+
+COIN_NAME = {
+    'Bitcoin': 'BTC',
+    'Ethereum': 'ETH',
+    'Cardano': 'ADA',
+    'Enjin Coin': 'ENJ', 
+    'Ripple': 'XRP',
+    'Polygon': 'MATIC',
+    'Solana': 'SOL'
+}
+
+COIN_LINK = {
+    'Bitcoin': 'https://coinmarketcap.com/currencies/bitcoin/',
+    'Ethereum': 'https://coinmarketcap.com/currencies/ethereum/',
+    'Cardano': 'https://coinmarketcap.com/currencies/cardano/',
+    'Enjin Coin': 'https://coinmarketcap.com/currencies/enjin-coin/', 
+    'Ripple': 'https://coinmarketcap.com/currencies/xrp/',
+    'Polygon': 'https://coinmarketcap.com/currencies/polygon/',
+    'Solana': 'https://coinmarketcap.com/currencies/solana/'
+}
+
+for crypto in CRYPTO_LIST:
+    assert COIN_NAME.get(crypto) is not None, f'No coin name for crypto {crypto}'
+    assert COIN_LINK.get(crypto) is not None, f'No coin link for crypto {crypto}'
+
---
+++ scan.py
@@ -0,0 +1,70 @@
+from typing import NoReturn
+import requests
+from bs4 import BeautifulSoup
+from config import CRYPTO_LIST, COIN_LINK, COIN_NAME
+from prettytable import PrettyTable
+from threading import Timer
+import json 
+import time
+
+def normalize_price(price: str):
+    price = price[1:].replace(',', '')
+    return float(price)
+
+def normalize_to_bitcoin(to_bitcoin: str):
+    price = to_bitcoin.split()[0].replace(',', '')
+    return float(price)
+
+
+def get_info( crypto, link, conversions ):
+    page = requests.get(link)
+    if page.status_code != 200:
+        print(f"Couldn't parse {link}")
+        return None, None 
+    #print(f"{crypto} Downloaded")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    summary_container = soup.find("div", class_='priceSection')
+    try:
+        price = normalize_price( summary_container.select('.priceTitle .priceValue span')[0].get_text() )
+    except Exception as e:
+        print(e)
+        price = None 
+    
+    try:
+        target_key = (COIN_NAME[crypto] + COIN_NAME['Bitcoin']).upper()
+        to_bitcoin = 1
+        if crypto != 'Bitcoin':
+            to_bitcoin = conversions.get(target_key)
+        bitcoin_to_crypto = (1.0/to_bitcoin)*0.995
+    except Exception as e:
+        print(e)
+        bitcoin_to_crypto = None
+    return price, bitcoin_to_crypto
+
+
+def print_table():
+    #Timer(30, print_table).start()
+    while True:
+        table = PrettyTable()
+        table.add_column("Info", ["$Price", "From BTC (NiceHash)"])
+        conversions_response = requests.get("https://api2.nicehash.com/exchange/api/v2/info/prices")
+        conversions = None 
+        if conversions_response.status_code == 200:
+            conversions = json.loads(conversions_response.text)
+        for crypto in CRYPTO_LIST:
+            price, from_bitcoin = get_info( crypto, COIN_LINK[crypto], conversions )
+            price_pretty = "-1"
+            if price is not None:
+                price_pretty = f"{price:.2f}"
+            from_bitcoin_pretty = "-1"
+            if from_bitcoin is not None:
+                from_bitcoin_pretty = f"{from_bitcoin:.2f}"
+            table.add_column(f"{crypto}", [price_pretty, from_bitcoin_pretty])
+            table.align[crypto] = "r"
+        print(table)
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    print_table()
+    
